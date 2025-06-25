2b5c8671c418393ed06d9dd613707116d4ee7149
alexandre fatec <alexandre.ribeiro21@fatec.sp.gov.br>
2025-06-25 13:49:16 -0300

    Componentes criados na barra de navegacao

--- src/componentes/roteador.tsx
+++ src/componentes/roteador.tsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import BarraNavegacao from "./barraNavegacao";
-import FormularioCadastroCliente from "./formularioCadastroCliente";
-import ListaCliente from "./listaCliente";
+import FormularioCadastroCliente from "./cadastro/formularioCadastroCliente";
+import ListaCliente from "./clientes/listaCliente";
 
 type state = {
     tela: string
@@ -18,29 +18,57 @@ export default class Roteador extends Component<{}, state> {
 
     selecionarView(novaTela: string, evento: Event) {
         evento.preventDefault()
-        console.log(novaTela);
+        console.log("Mudando para:", novaTela)
         this.setState({
             tela: novaTela
         })
     }
 
     render() {
-        let barraNavegacao = <BarraNavegacao seletorView={this.selecionarView} tema="purple lighten-4" botoes={['Clientes', 'Cadastros']} />
-        if (this.state.tela === 'Clientes') {
-            return (
-                <>
-                    {barraNavegacao}
-                    <ListaCliente tema="purple lighten-4" />
-                </>
-            )
-        } else {
-            return (
-                <>
-                    {barraNavegacao}
-                    <FormularioCadastroCliente tema="purple lighten-4" />
-                </>
-            )
-        }
+        let barraNavegacao = (
+            <BarraNavegacao
+                seletorView={this.selecionarView}
+                tema="purple lighten-4"
+                botoes={['Clientes', 'Cadastros', 'Produtos', 'Servicos']}
+            />
+        )
 
+        switch (this.state.tela) {
+            case 'Clientes':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <ListaCliente tema="purple lighten-4" />
+                    </>
+                )
+            case 'Cadastros':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <FormularioCadastroCliente tema="purple lighten-4" />
+                    </>
+                )
+            case 'Produtos':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <div className="container"><h5>Cadastro de Produtos (em breve)</h5></div>
+                    </>
+                )
+            case 'Servicos':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <div className="container"><h5>Cadastro de Serviços (em breve)</h5></div>
+                    </>
+                )
+            default:
+                return (
+                    <>
+                        {barraNavegacao}
+                        <div className="container"><h5>Bem-vindo!</h5></div>
+                    </>
+                )
+        }
     }
 }
